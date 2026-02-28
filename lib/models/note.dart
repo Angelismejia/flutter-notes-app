f7f7360b3ada9935b6d549337e05069d1080d213
Angelismejia <Angelismejia06@gmail.com>
2026-02-28 01:13:16 -0400

    feat: add Note model

---
+++ lib/models/note.dart
@@ -0,0 +1,35 @@
+class Note {
+  int? id;
+  String title;
+  String content;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  Note({
+    this.id,
+    required this.title,
+    required this.content,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'content': content,
+      'createdAt': createdAt.millisecondsSinceEpoch,
+      'updatedAt': updatedAt.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'],
+      title: map['title'] ?? '',
+      content: map['content'] ?? '',
+      createdAt: DateTime.fromMillisecondsSinceEpoch(map['createdAt']),
+      updatedAt: DateTime.fromMillisecondsSinceEpoch(map['updatedAt']),
+    );
+  }
+}
